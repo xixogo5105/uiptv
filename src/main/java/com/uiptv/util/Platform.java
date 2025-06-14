fc5f1fbfac2abbe22863c85d807c8ecf7407dc2a
Mickaël Salmon <mickael@humbkr.uk>
2025-06-14 19:10:18 +0200

    fix(app): allow passing arguments to external player
    Signed-off-by: Mickaël Salmon <mickael.salmon@semarchy.com>

--- src/main/java/com/uiptv/util/Platform.java
+++ src/main/java/com/uiptv/util/Platform.java
@@ -1,15 +1,23 @@
 package com.uiptv.util;
 
+import com.uiptv.ui.LogDisplayUI;
+
 import java.io.File;
 
-import static com.uiptv.util.SystemUtils.*;
+import static com.uiptv.util.SystemUtils.IS_OS_MAC_OSX;
+import static com.uiptv.util.SystemUtils.IS_OS_WINDOWS;
 
 public class Platform {
+
+    private Platform() {
+        super();
+    }
+
     public static void executeCommand(String command, String args) {
-        if (IS_OS_WINDOWS) {
-            WindowsPlatform.executeCommand(command, args);
-        } else if (IS_OS_LINUX || IS_OS_MAC_OSX) {
-            LinuxPlatform.executeCommand(command, args);
+        try {
+            new CmdLineRunner().exec(command + " " + args);
+        } catch (CmdLineRunner.CmdLineException e) {
+            LogDisplayUI.addLog("Error occurred while executing commands. Error Description: " + e.getMessage());
         }
     }
 
