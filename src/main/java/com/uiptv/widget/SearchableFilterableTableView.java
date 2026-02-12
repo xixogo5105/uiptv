0f1d68c58a36c3db9b4b311b335716b684226e36
xixogo5105 <xixogo5105@protonmail.com>
2026-02-12 18:59:40 +0000

    Misc bug fixes

--- src/main/java/com/uiptv/widget/SearchableFilterableTableView.java
+++ src/main/java/com/uiptv/widget/SearchableFilterableTableView.java
@@ -2,67 +2,81 @@ package com.uiptv.widget;
 
 import com.uiptv.ui.AccountListUI;
 import com.uiptv.util.AccountType;
+import javafx.beans.Observable;
 import javafx.beans.binding.Bindings;
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.transformation.FilteredList;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import static com.uiptv.ui.RootApplication.GUIDED_MAX_WIDTH_PIXELS;
 import static com.uiptv.util.AccountType.getAccountTypeByDisplay;
 
 public class SearchableFilterableTableView extends TableView {
-    public static final String ALL = "All";
     private final UIptvText textField = new UIptvText("search" + new Date().getTime(), "Search", 10);
-    private final ComboBox comboBox = new ComboBox();
+    private final MenuButton menuButton = new MenuButton("All");
+    private final List<CheckMenuItem> checkMenuItems = new ArrayList<>();
 
     public SearchableFilterableTableView() {
         this.setPrefWidth((double) GUIDED_MAX_WIDTH_PIXELS / 3);
         this.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
         textField.setOnMousePressed(event -> textField.clear());
-        comboBox.setPrefWidth(175);
+        menuButton.setPrefWidth(175);
         textField.setPrefWidth(275);
-        List<String> list = Arrays.stream(AccountType.values()).map(AccountType::getDisplay).toList();
-        comboBox.getItems().add(ALL);
-        comboBox.getItems().addAll(list);
-        comboBox.setValue(ALL);
 
+        for (AccountType type : AccountType.values()) {
+            CheckMenuItem item = new CheckMenuItem(type.getDisplay());
+            item.selectedProperty().addListener((obs, wasSelected, isSelected) -> updateMenuButtonText());
+            menuButton.getItems().add(item);
+            checkMenuItems.add(item);
+        }
+    }
+
+    private void updateMenuButtonText() {
+        String selectedItemsText = checkMenuItems.stream()
+                .filter(CheckMenuItem::isSelected)
+                .map(MenuItem::getText)
+                .collect(Collectors.joining(", "));
+
+        if (selectedItemsText.isEmpty()) {
+            menuButton.setText("All");
+        } else {
+            menuButton.setText(selectedItemsText);
+        }
     }
 
     public TextField getTextField() {
         return textField;
     }
 
-    public ComboBox getComboBox() {
-        return comboBox;
+    public MenuButton getMenuButton() {
+        return menuButton;
     }
 
     public <T> void filterByAccountType() {
-
-
-        ObjectProperty<Predicate<AccountListUI.AccountItem>> textFilter = new SimpleObjectProperty<>();
-        ObjectProperty<Predicate<AccountListUI.AccountItem>> comboFilter = new SimpleObjectProperty<>();
-
-        textFilter.bind(Bindings.createObjectBinding(() -> accountItem -> accountItem.getAccountName().toLowerCase().contains(textField.getText().toLowerCase()),
-                textField.textProperty()));
-
-
-        comboFilter.bind(Bindings.createObjectBinding(() -> accountItem -> comboBox.getValue().toString().equalsIgnoreCase(ALL) || accountItem.getAccountType().equalsIgnoreCase(getAccountTypeByDisplay(comboBox.getValue().toString()).name()),
-                comboBox.valueProperty()));
-
         FilteredList<AccountListUI.AccountItem> filteredItems = new FilteredList<>(FXCollections.observableList(getItems()));
         setItems(filteredItems);
 
-        filteredItems.predicateProperty().bind(Bindings.createObjectBinding(
-                () -> textFilter.get().and(comboFilter.get()),
-                textFilter, comboFilter));
+        List<Observable> dependencies = new ArrayList<>();
+        dependencies.add(textField.textProperty());
+        checkMenuItems.forEach(item -> dependencies.add(item.selectedProperty()));
+
+        filteredItems.predicateProperty().bind(Bindings.createObjectBinding(() -> {
+            String searchText = textField.getText() == null ? "" : textField.getText().toLowerCase();
+            List<String> selectedTypes = checkMenuItems.stream()
+                    .filter(CheckMenuItem::isSelected)
+                    .map(item -> getAccountTypeByDisplay(item.getText()).name())
+                    .collect(Collectors.toList());
+
+            return accountItem -> {
+                boolean matchesText = accountItem.getAccountName().toLowerCase().contains(searchText);
+                boolean matchesType = selectedTypes.isEmpty() || selectedTypes.contains(accountItem.getAccountType());
+                return matchesText && matchesType;
+            };
+        }, dependencies.toArray(new Observable[0])));
     }
 }
