c34450a7d5a54925c7adedbbd27caad5b9f61e03
xixogo5105 <xixogo5105@protonmail.com>
2026-06-01 20:33:35 +0100

    Remove unused catalog import

--- core/src/main/java/com/uiptv/application/CatalogApplicationService.java
+++ core/src/main/java/com/uiptv/application/CatalogApplicationService.java
@@ -36,7 +36,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 
